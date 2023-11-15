76922f5f283dada276fda31ff16c7bfab1c5d919
CazierFlavien <flavien.cazier@gmail.com>
2023-11-15 10:12:09 +0100

    Ex1

---
+++ main.js
@@ -0,0 +1,7 @@
+var MavariableString = "toto";
+var MavariableNumerique= 100;
+var MavariableTableau = ['Apple', 'Banana'];
+var Mavariableobjet = window
+alert(MavariableString + MavariableNumerique + MavariableTableau[0]+ 
+    Mavariableobjet.location.pathname);
+
