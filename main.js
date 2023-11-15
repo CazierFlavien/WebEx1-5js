0d369429fd60b4fd4c1072579d29a9ecbe574847
CazierFlavien <flavien.cazier@gmail.com>
2023-11-15 11:22:28 +0100

    ex4

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 var MavariableString = "toto";
-var MavariableNumerique= 100;
-var MavariableTableau = ['Apple', 'Banana'];
-var Mavariableobjet = window
-alert(MavariableString + MavariableNumerique + MavariableTableau[0]+ 
-    Mavariableobjet.location.pathname);
 
-var MavariableTailleFenetre = window.innerWidth;
-window.addEventListener('resize', () => {
-    alert((MavariableTailleFenetre = window.innerWidth));
-});
+var MavariableNumerique = prompt("Entre un nombre",100);
+
+if (MavariableString == "toto" && MavariableNumerique !=200 || MavariableNumerique <=100){
+    alert("je suis le cas 1");
+
+}else{
+    alert("je suis le cas 2");
+}
