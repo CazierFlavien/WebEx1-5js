7c0ab131d2d64edb67742499a69861860bd7f592
CazierFlavien <flavien.cazier@gmail.com>
2023-12-13 09:07:43 +0100

    Ex11

--- main.js
+++ main.js
@@ -1,41 +1,46 @@
-/*function factoriel(n) {
-  if (n === 0 || n === 1) {
-    return 1;
-  } else {
-    return n + factoriel(n - 1);
-  }
+var compteur = 0;
+var compteur2 = 0;
+
+var Madiv = document.getElementById("Methode2");
+Madiv.addEventListener("click", UneProcedureQuiChangeLetext);
+
+Madiv = document.getElementById("Methode3");
+Madiv.addEventListener("mouseover", UneProcedureQuiSurvole);
+
+Madiv = document.getElementById("Methode4");
+Madiv.addEventListener("keydown", UneProcedureQuiEcrit);
+
+function UneProcedureQuiEcrit(evenement) {
+    evenement.target.innerHTML = "On a appuyé sur : " + evenement.key;
 }
 
-let nombre = prompt("Veuillez saisir un nombre pour calculer sa factorielle :");
+function UneProcedureQuiChangeLetext(evenement) {
+    compteur++;
+    evenement.target.innerHTML = "On m'a cliqué dessus " + compteur + " fois";
+}
 
+function UneProcedureQuiSurvole(evenement) {
+    compteur2++;
+    evenement.target.innerHTML = "On m'a survolé " + compteur2 + " fois";
+}
 
-if (!isNaN(nombre)) {
-  let n = parseInt(nombre);
-  if (n >= 0) {
-    let resultat = factoriel(n);
-    alert("La factorielle de " + n + " est : " + resultat);
-  } else {
-    alert("Veuillez saisir un nombre positif pour calculer la factorielle.");
-  }
-} else {
-  alert("Ce n'est pas un nombre valide.");
-}*/
-var somme =0;
-let nombre = prompt("Veuillez saisir un nombre pour calculer sa factorielle :");
+var monDiv = document.getElementById("monDiv");
 
-for (let i = 1; i <nombre ; i++) {
-  //somme =i + somme ;
-  somme = addition(i,somme);
-  console.log(somme);
-  
+monDiv.addEventListener("click", function() {
+    monDiv.className = "nouvelleClasse";
+});
+
+function changerVisibilite(visible) {
+    var div = document.getElementById("monDiv2");
+    if (visible) {
+        div.style.visibility = "visible";
+    } else {
+        div.style.visibility = "hidden";
+    }
 }
 
 
 
 
-function addition(a,b){
-  let c = 0;
-  c = a + b
-  return c;
 
-}
+
