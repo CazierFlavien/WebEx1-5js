6048fbc3a95c4bf5fe6882eac29a1e010a9c9a4b
CazierFlavien <flavien.cazier@gmail.com>
2023-12-20 08:33:53 +0100

    ex11

--- main.js
+++ main.js
@@ -1,43 +1,11 @@
-var compteur = 0;
-var compteur2 = 0;
-
-var Madiv = document.getElementById("Methode2");
-Madiv.addEventListener("click", UneProcedureQuiChangeLetext);
-
-Madiv = document.getElementById("Methode3");
-Madiv.addEventListener("mouseover", UneProcedureQuiSurvole);
-
-Madiv = document.getElementById("Methode4");
-Madiv.addEventListener("keydown", UneProcedureQuiEcrit);
-
-function UneProcedureQuiEcrit(evenement) {
-    evenement.target.innerHTML = "On a appuyé sur : " + evenement.key;
-}
-
-function UneProcedureQuiChangeLetext(evenement) {
-    compteur++;
-    evenement.target.innerHTML = "On m'a cliqué dessus " + compteur + " fois";
-}
-
-function UneProcedureQuiSurvole(evenement) {
-    compteur2++;
-    evenement.target.innerHTML = "On m'a survolé " + compteur2 + " fois";
-}
-
-var monDiv = document.getElementById("monDiv");
-
-monDiv.addEventListener("click", function() {
-    monDiv.className = "nouvelleClasse";
-});
-
-function changerVisibilite(visible) {
-    var div = document.getElementById("monDiv2");
-    if (visible) {
-        div.style.visibility = "visible";
-    } else {
-        div.style.visibility = "hidden";
-    }
-}
+fetch(
+    "http://192.168.65.191/test.json",
+    {method: `GET`}
+).then(
+response => response.json()
+).then(
+    response2 => console.log(JSON.stringify(response2))
+).catch(error => console.log("Erreur : " + error));
 
 
 
